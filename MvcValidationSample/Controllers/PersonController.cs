9e7b1b3fde880441e921671a9db9ccd1213c3312
Kuei <ragnakuei@livemail.tw>
2020-10-12 19:44:58 +0800

    add View by scaffold

---
+++ MvcValidationSample/Controllers/PersonController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MvcValidationSample.Models;
+
+namespace MvcValidationSample.Controllers
+{
+    public class PersonController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(PersonViewModel personViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                TempData["Result"] = "新增完畢";
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View();
+        }
+    }
+}
---
+++ MvcValidationSample/Models/PersonViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcValidationSample.Models
+{
+    public class PersonViewModel
+    {
+        [Display(Name             = "編號")]
+        [Required(ErrorMessage    = "{0} 為必填")]
+        [Range(2, 5, ErrorMessage = "{0} 數值 要介於 {1} 及 {2} 之間")]
+        public int? Id { get; set; }
+
+        [Display(Name = "名稱")]
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage         = "請輸入{0}")]
+        [StringLength(5, MinimumLength = 2, ErrorMessage = "{0} 長度 要介於 {2} 及 {1} 之間")]
+        public string Name { get; set; }
+
+        [Display(Name = "電子信箱")]
+        [Required(ErrorMessage     = "{0} 為必填")]
+        [EmailAddress(ErrorMessage = "{0} 格式錯誤")]
+        public string Email { get; set; }
+    }
+}
